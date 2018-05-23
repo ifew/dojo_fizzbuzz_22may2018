a7db49e1d414dbbf6979ac7ef130ae81e763896b
iFew <chitpong@gmail.com>
2018-05-23 11:10:08 +0700

    Added code and tests from practitioners

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -2,8 +2,32 @@ using System;
 
 namespace dojo_fizzbuzz_22may2018
 {
-    public class FizzBuzz
+    internal class FizzBuzz
     {
-        
+        public FizzBuzz()
+        {
+        }
+
+        internal string say(int ask)
+        {
+            if(หาร3ลงตัว(ask) && หาร5ลงตัว(ask)){
+                return "FIZZBUZZ";
+            }
+            else if(หาร3ลงตัว(ask)){
+                return "FIZZ";
+            }else if(หาร5ลงตัว(ask)){
+                return "BUZZ";
+            }
+            return ask.ToString();
+        }
+
+        private bool หาร3ลงตัว(int ask)
+        {
+            return (ask % 3) == 0;
+        }
+          private bool หาร5ลงตัว(int ask)
+        {
+            return (ask % 5) == 0;
+        }
     }
 }
--- FizzBuzzTest.cs
+++ FizzBuzzTest.cs
@@ -5,10 +5,104 @@ namespace dojo_fizzbuzz_22may2018
 {
     public class FizzBuzzTest
     {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        
         [Fact]
-        public void Test1()
+        public void When_Ask1_Should_Be_Say1()
         {
-            
+            var expected = "1";
+            var say = 1;
+
+            var result = fizzBuzz.say(say);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void When_Ask2_Should_Be_Say2()
+        {
+            var expected = "2";
+            var ask = 2;
+
+            var result = fizzBuzz.say(ask);
+
+            Assert.Equal(expected, result);
+        }
+        
+        [Fact]
+        public void When_Ask3_Should_Be_SayFIZZ()
+        {
+            var expected = "FIZZ";
+            var ask = 3;
+
+            var result = fizzBuzz.say(ask);
+
+            Assert.Equal(expected, result);
+        }        
+        
+        [Fact]
+        public void When_Ask4_Should_Be_Say4()
+        {
+            var expected = "4";
+            var ask = 4;
+
+            var result = fizzBuzz.say(ask);
+
+            Assert.Equal(expected, result);
+        }
+        
+        [Fact]
+        public void When_Ask5_Should_Be_SayBUZZ()
+        {
+            var expected = "BUZZ";
+            var ask = 5;
+
+            var result = fizzBuzz.say(ask);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void When_Ask6_Should_Be_SayFIZZ()
+        {
+            var expected = "FIZZ";
+            var ask = 6;
+
+            var result = fizzBuzz.say(ask);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void When_Ask9_Should_Be_SayFIZZ()
+        {
+            var expected = "FIZZ";
+            var ask = 9;
+
+            var result = fizzBuzz.say(ask);
+
+            Assert.Equal(expected, result);
+        }
+         [Fact]
+        public void When_Ask10_Should_Be_SayBUZZ()
+        {
+            var expected = "BUZZ";
+            var ask = 10;
+
+            var result = fizzBuzz.say(ask);
+
+            Assert.Equal(expected, result);
+        }
+        
+         [Fact]
+        public void When_Ask15_Should_Be_SayFIZZBUZZ()
+        {
+            var expected = "FIZZBUZZ";
+            var ask = 15;
+
+            var result = fizzBuzz.say(ask);
+
+            Assert.Equal(expected, result);
         }
     }
 }
